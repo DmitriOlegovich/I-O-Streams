9b386d5aee7b6a98d2934843d918129948ce134d
Dmitriy Prosolov <prosolovdmitri@mail.ru>
2023-04-19 18:13:41 +0300

    commit json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -7,21 +8,23 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     static String[] products = {"Молоко", "Хлеб", "Гречка"};
     static int[] prices = {78, 50, 64};
-    static File saveFile = new File("basket.bin");
+    static File saveFile = new File("basket.json");
     public static void main(String[] args) throws FileNotFoundException {
 
         Basket basket = null;
         if(saveFile.exists()){
-            basket = Basket.loadFromBinFile(saveFile);
+            basket = Basket.loadFromJSONFile(saveFile);
         } else {
             basket = new Basket(products, prices);
         }
 
+        ClientLog log = new ClientLog();
         while (true) {
             showPrice();
             System.out.println("Выберите товар и количество через пробел или введите end.");
             String input = scanner.nextLine();
             if ("end".equals(input)) {
+                log.exportAsCSV(new File("log.csv"));
                 break;
             }
 
@@ -29,7 +32,8 @@ public class Main {
             int productNumber = Integer.parseInt(parts[0]) - 1;
             int productCount = Integer.parseInt(parts[1]);
             basket.addToCart(productNumber, productCount);
-            basket.saveBin(saveFile);
+            log.log(productNumber, productCount);
+            basket.saveJSON(saveFile);
 
         }
         basket.printCart();
